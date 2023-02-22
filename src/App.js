cd0d00816e34c61b02c33d09026a190e2201b86d
devPrzemyslawT <przemyslaw.tomaszewski.dev@gmail.com>
2023-02-22 11:45:08 +0100

    simple structure of application created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<h1> Breaking React News </h1>
+				<img src={logo} className='App-logo' alt='logo' />
+			</div>
+			<div className='App-body'>
+				<h2> Always in the right place at the right time! </h2>
+				<h3> www.reactnews.reactnews </h3>
+			</div>
+		</div>
+	);
 }
 
 export default App;
